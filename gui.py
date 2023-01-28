4d18ca3377cd36ba1f184175a9c3cf2580bb68b1
Pavlo Bydionnyi <klaviatyra101@gmail.com>
2023-01-28 03:40:05 +0200

    update using ling update name video

--- gui.py
+++ gui.py
@@ -1,6 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
+import ffmpeg
+from pytube import YouTube
+from pytube.cli import on_progress
+import os
+
+#os.remove('video.mp4')
+#os.remove('audio.mp3')
+
 root = Tk()
 root.title('YouTube Downloader')
 root.iconbitmap(default='favicon.ico')
@@ -9,11 +17,16 @@ root.resizable(False, False)
 
 ##################
 
-def click():
-    print('hello')
 
 
 ##################
+def get_url():
+    url = link.get()
+    yt = YouTube(url)
+    nam['text'] = yt.title
+    print(url)
+
+
 
 link = ttk.Entry()
 link.pack(side='top', pady=5, ipady=5,)
@@ -23,6 +36,10 @@ link.place(x=110, y=8, height=25, width=280)
 label = ttk.Label(text='Enter link:', font=('Intro', 12))
 label.pack(side='top')
 label.place(x=4, y=9)
+
+fnd = ttk.Button(text='Find', command=get_url)
+fnd.pack(side=TOP)
+fnd.place(x=165, y=40)
 
 hd = 'HD'
 fhd = 'Full HD'
@@ -45,17 +62,17 @@ uhd_q = ttk.Radiobutton(text='2160p', value=uhd)
 uhd_q.pack()
 uhd_q.place(x=230, y=150)
 
-dwnl = ttk.Button(text='Download', command=click)
+dwnl = ttk.Button(text='Download')
 dwnl.pack(side=BOTTOM, fill='x', padx=10, pady=10, ipady=10)
-
-fnd = ttk.Button(text='Find')
-fnd.pack(side=TOP)
-fnd.place(x=165, y=40)
 
 bar = ttk.Progressbar()
 bar.pack(side=BOTTOM, fill='x', padx=10)
 
-nam = Label(text='Name: ')
+nam0 = Label(text='Name: ')
+nam0.pack()
+nam0.place(x=180, y=375)
+
+nam = Label()
 nam.pack(side=BOTTOM)
 
 #########################
