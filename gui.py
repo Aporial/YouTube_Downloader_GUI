3ddb4d3a4c7095c0e2bc4b761bb81e6899c74d6e
Pavlo Bydionnyi <klaviatyra101@gmail.com>
2023-01-28 03:09:20 +0200

    update test gui

--- gui.py
+++ gui.py
@@ -7,35 +7,58 @@ root.iconbitmap(default='favicon.ico')
 root.geometry('400x500+750+250')
 root.resizable(False, False)
 
+##################
+
+def click():
+    print('hello')
+
+
+##################
+
 link = ttk.Entry()
 link.pack(side='top', pady=5, ipady=5,)
-link.place(x=93, y=8, height=25, width=300)
+link.place(x=110, y=8, height=25, width=280)
 
-label = ttk.Label(text='Enter link:')
+
+label = ttk.Label(text='Enter link:', font=('Intro', 12))
 label.pack(side='top')
-label.place(x=10, y=10)
+label.place(x=4, y=9)
 
-qua = ttk.Checkbutton(text='720p')
-qua.pack()
-qua.place(x=5, y=50)
+hd = 'HD'
+fhd = 'Full HD'
+twok = '2K'
+uhd = '4K'
 
-qua = ttk.Checkbutton(text='1080p')
-qua.pack()
-qua.place(x=100, y=50)
+hd_q = ttk.Radiobutton(text='720p', value=hd)
+hd_q.pack()
+hd_q.place(x=120, y=80)
 
-qua = ttk.Checkbutton(text='1440p')
-qua.pack()
-qua.place(x=5, y=100)
+fhd_q = ttk.Radiobutton(text='1080p', value=fhd)
+fhd_q.pack()
+fhd_q.place(x=230, y=80)
 
-qua = ttk.Checkbutton(text='2160p')
-qua.pack()
-qua.place(x=100, y=100)
+twok_q = ttk.Radiobutton(text='1440p', value=twok)
+twok_q.pack()
+twok_q.place(x=120, y=150)
 
-btn = ttk.Button(text='Download')
-btn.pack(side=BOTTOM, fill='x', padx=10, pady=10, ipady=10)
+uhd_q = ttk.Radiobutton(text='2160p', value=uhd)
+uhd_q.pack()
+uhd_q.place(x=230, y=150)
 
+dwnl = ttk.Button(text='Download', command=click)
+dwnl.pack(side=BOTTOM, fill='x', padx=10, pady=10, ipady=10)
 
+fnd = ttk.Button(text='Find')
+fnd.pack(side=TOP)
+fnd.place(x=165, y=40)
 
+bar = ttk.Progressbar()
+bar.pack(side=BOTTOM, fill='x', padx=10)
+
+nam = Label(text='Name: ')
+nam.pack(side=BOTTOM)
+
+#########################
 
 
 
